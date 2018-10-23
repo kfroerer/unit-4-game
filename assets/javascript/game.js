6c28d4292e83088e0a55904a5722f7352b33368c
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-10-22 21:27:56 -0500

    random gem value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,45 @@
 var wins = 0;
 var losses = 0;
 var userScore = 0;
-var targetMin = 19
-var targetMax = 120;
-var gemMin = 1;
-var gemMax = 12;
-
-var targetNum = myFunction(targetMin, targetMax);
-
-function myFunction(targetMin, targetMax){
-    return Math.floor(Math.random() * (targetMax-targetMin +1) + targetMin);
+var gems = [];
+var a = gems[0];
+var b = gems[1];
+var c = gems[2];
+var d = gems[3];
+//generate target number
+var targetNum = myFunction();
+function myFunction(){
+    return Math.floor(Math.random() * (102) + 19);
 };
 console.log(targetNum)
+//generate 4 random numbers for gems 
+for (var i = 0; i < 4; i++) {
+    var a = Math.floor(Math.random() * (12) + 1);
+    gems[i] = a;
+    console.log(gems);
+}
 
+$(document).ready(function() {
 
-var gemValue = random(gemMin, gemMax);
-function random (gemMin, gemMax){
-    return Math.floor(Math.random() * (gemMax-gemMin +1) + gemMin);
-};
-console.log(gemValue)
-// $("#newGame").on("click", function(){
-//     $("target-number").text.targetNum;
-// })
-// $(".col-md-3").on("click", function (){
+$("#newGame").on("click", function(){
+    $("#target-number").text(targetNum);      
+});
+
+//TODO on click gem, pull value from array
+// add that value to the user score
+// add if/then statements to update wins/losses/reset the game
+ 
+    
+    
+       
+    // $("#gem2").on("click", function (){
+    //     $('#userScore').text(gemValue);
+    // })
+    // $("#gem3").on("click", function (){
+    //     $('#userScore').text(gemValue);
+    // })
+    // $("#gem4").on("click", function (){
+    //     $('#userScore').text(gemValue);
+    // })
+})
      
